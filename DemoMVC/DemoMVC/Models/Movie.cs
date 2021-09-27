45c8d91ebe5e8a46f26d0ad6e96e02874ed72d53
Tran Phan Kim Son <spk@elcaNet.local>
2021-09-27 22:13:53 +0700

    Initial

---
+++ DemoMVC/DemoMVC/Models/Movie.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using NewResources;
+namespace DemoMVC.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        [MinLength(5, ErrorMessageResourceType = typeof(NewResources.Resources), ErrorMessageResourceName ="TooShort")]
+        [Display(Name = "Title")]
+        public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class MovieDBContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
